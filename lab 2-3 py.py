43288db74342383bc9cb5dcc75f06d793fb02ce1
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-09-11 13:33:49 -0400

    labs
    2-3

---
+++ lab 2-3 py.py
@@ -0,0 +1,10 @@
+# author lp 2-3
+a = 2
+print(f"Type of a (initial): {type(a)}")
+a = float(a)
+print(f"Type of a after converting to float: {type(a)}")
+a = str(a)
+print(f"Type of a after converting to string: {type(a)}")
+a = bool(a)
+print(f"Type of a after converting to Boolean: {type(a)}")
+print(f"a == 2: {a == 2}") 
