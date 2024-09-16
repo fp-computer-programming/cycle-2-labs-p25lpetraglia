97ed948536407c1c4d4575f9baa87cdfe359fb30
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-09-16 14:09:26 -0400

    labs
    2-5

---
+++ # author lp 2-5.py
@@ -0,0 +1,7 @@
+# author lp 2-5
+a = 15
+b = 8
+sum_result = a + b
+sum_difference = a - b
+print(f"Sum of a and b: {sum_result}")
+print(f"Difference when b is subtracted from a: {difference_result}")
