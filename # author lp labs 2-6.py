372a16f91c42e1e8b60c5066196be0215f9e3df5
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-09-17 08:53:10 -0400

    labs
    2-6

---
+++ # author lp labs 2-6.py
@@ -0,0 +1,8 @@
+# author lp labs 2-6
+a = 1
+b = 2 
+c = 3
+x = 2
+y = 5
+result = (3 + 4 * x) / 5 - 10 * ((y - 5) * (a + b + c)) / x + 9 * ((4 / x**2) + (9 + x) / y)
+print  (f"resulrt of the expression: {result}")
