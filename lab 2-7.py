a8564b7da682ddc9c1b12eddab573e52ae9bcf0b
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-09-19 09:51:59 -0400

    lab 2-7

---
+++ lab 2-7.py
@@ -0,0 +1,12 @@
+lab 2-7 
+# author lp 
+# a would be an error because it is a type error
+int(a)
+# ('a') would be an error because you do not need apostrophies inside parenthesis (syntax error)
+int('a')
+# you cannot add an integer into a string so it would be an error  (syntax error)
+'a' + 2
+# there is no date it is only datetime (module error)
+import date
+#  error due to a missing closing quotation mark (syntax error).
+print("I'm a happy camper!)
