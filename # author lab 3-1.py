5d8cde7f76a805cd5053a9b29f98e0b78a702965
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-10-01 13:30:33 -0400

    lab 3-1

---
+++ # author lab 3-1.py
@@ -0,0 +1,28 @@
+# author lab 3-1
+import math
+
+print("1. math.ceil(7.15):", math.ceil(7.15))
+
+
+print("2. math.floor(19.156):", math.floor(19.156))
+
+
+print("3. math.fabs(-42):", math.fabs(-42))
+
+
+print("4. math.pow(2, 3):", math.pow(2, 3))
+
+print("5. math.log10(100):", math.log10(100))
+
+
+print("6. math.sqrt(9):", math.sqrt(9))
+
+print("7. math.isqrt(2):", math.isqrt(2))
+
+print("8. math.factorial(5):", math.factorial(5))
+
+
+print("9. math.gcd(111111111, 12321):", math.gcd(111111111, 12321))
+
+
+print("10. math.sin(0):", math.sin(0))
