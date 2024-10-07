04eea6a818c85127f1755413167dc8e2317c6965
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-10-07 11:17:41 -0400

    lab 4-1

--- # author lp 4-1.py
+++ # author lp 4-1.py
@@ -1,17 +1,19 @@
 # author lp 4-1
+
 first_name = input("Please enter your first name: ")
 
 
 last_name = input("Please enter your last name: ")
+
 
 full_name = first_name + last_name
 
 
 print("Your full name is:", full_name)
 
-#first name and last name without a space
-# add a space between the first and last name.
+#first name and last name without a space in between.
+#  add a space between the first and last name.
 full_name_fixed = first_name + " " + last_name
 
-e
+
 print("Your full name with a space is:", full_name_fixed)
---
+++ # author lp labs 4-2.py
@@ -0,0 +1,15 @@
+# author lp labs 4-2
+
+school = "Fairfield Prep"
+
+
+first_half = school[:9] 
+second_half = school[9:]  
+
+
+print(first_half)
+print(second_half)
+
+
+full_name = first_half + second_half
+print(full_name)
