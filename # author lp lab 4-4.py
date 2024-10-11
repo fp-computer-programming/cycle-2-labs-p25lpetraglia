39abb20bb0376565711e86d7fa8a4b5084adbde3
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-10-11 09:24:41 -0400

    4-4

---
+++ # author lp lab 4-4.py
@@ -0,0 +1,9 @@
+# author lp lab 4-4
+
+person1 = input("Enter the first user's first name: ")
+
+
+person2 = input("Enter the second user's first name: ")
+
+
+print("Hello, {}. My name is {}".format(person1, person2))
