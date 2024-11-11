5b1094885b593a53d760c90d2e6a61e6b330ba0b
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-11-11 10:29:36 -0500

    lab 6-1

---
+++ # lab 6-1.py
@@ -0,0 +1,12 @@
+# lab 6-1
+#logan p
+def star_pyramid():
+    
+    rows = int(input("Enter the number of rows for the pyramid: "))
+    
+   
+    for i in range(1, rows + 1):
+        print('*' * i)
+
+
+star_pyramid()
