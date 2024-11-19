9d94a88a3fd4bd8b77aa0428cc0dc62da51b3378
logan petraglia <p25lpetraglia@fairfieldprep.org>
2024-11-19 08:52:49 -0500

    lab 7-6

---
+++ #lab 7-6.py
@@ -0,0 +1,14 @@
+#lab 7-6
+
+
+def party_invitations():
+   
+    names = ["Alice", "Bob", "Charlie"]
+
+   
+    for name in names:
+        print(f"Hi {name}, You're invited to my party on Friday!")
+
+
+if __name__ == "__main__":
+    party_invitations()
